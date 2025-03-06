093bcaef3cfdd5c41b5915fa4b10def09b7199a4
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-06 17:34:17 -0600

    Exercise 03 complete
    Current code has many variables with methods, will try to shorten

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 
+    //instead of making new variables, I can chain the methods together
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
