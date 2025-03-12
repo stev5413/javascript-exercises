8569f1f2025fb00e2b9b472e3e08f09393466b77
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-12 13:47:29 -0500

    Completed 3-12-25

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+  //is there a better way to write expressions with multiple variables
+  let resultC = (tempF - 32) * (5/9);
+
+  let finalC = Math.round(resultC * 10) / 10;
+
+return finalC;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  let resultF = (tempC * (9/5)) + 32;
+
+  let finalF = Math.round(resultF * 10) / 10;
+
+  return finalF;
 };
 
 // Do not edit below this line
