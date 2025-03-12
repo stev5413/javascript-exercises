03dea1a02014cc169b7facc418663f2ff4b05e93
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-12 12:55:30 -0500

    Completed 3-12-2025

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,21 @@
 const leapYears = function(year) {
 
-    if () {
+    if (year % 400 === 0) {
+        //can return true or false (non-string) for a boolean value
+        return true;
     
     }
-
-    else () {
-
+    // could I use || in the first part 
+    else if (year % 4 === 0 && year % 100 !== 0) {
+    
+        return true;
     }
+    
+    else {
+
+        return false;
+    }
+
 };
 
 // Do not edit below this line
